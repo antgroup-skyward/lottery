dd9811c688df8f1bc2510d26414dafccc0a23626
badb100d <260448993@qq.com>
2023-12-15 10:41:15 +0800

    modified

--- js/member.js
+++ js/member.js
@@ -1,114 +1,108 @@
-var member = [
-  {
-    "phone": "",
-    "name": "栈长"
-  },
-  {
-    "phone": "",
-    "name": "库特"
-  },
-  {
-    "phone": "",
-    "name": "明程"
-  },
-  {
-    "phone": "",
-    "name": "空寻"
-  },
-  {
-    "phone": "",
-    "name": "干拉"
-  },
-  {
-    "phone": "",
-    "name": "骛望"
-  },
-  {
-    "phone": "",
-    "name": "晟一"
-  },
-  {
-    "phone": "",
-    "name": "戒律"
-  },
-  {
-    "phone": "",
-    "name": "织弦"
-  },
-  {
-    "phone": "",
-    "name": "承武"
-  },
-  {
-    "phone": "",
-    "name": "超六"
-  },
-  {
-    "phone": "",
-    "name": "艾许"
-  },
-  {
-    "phone": "",
-    "name": "亚诺"
-  },
-  {
-    "phone": "",
-    "name": "金鞍"
-  },
-  {
-    "phone": "",
-    "name": "沙匠"
-  },
-  {
-    "phone": "",
-    "name": "白焱"
-  },
-  {
-    "phone": "",
-    "name": "临野"
-  },
-  {
-    "phone": "",
-    "name": "飏洋"
-  },
-  {
-    "phone": "",
-    "name": "柒伍"
-  },
-  {
-    "phone": "",
-    "name": "头孢"
-  },
-  {
-    "phone": "",
-    "name": "宁齐"
-  },
-  {
-    "phone": "",
-    "name": "木绣"
-  },
-  {
-    "phone": "",
-    "name": "川枫"
-  },
-  {
-    "phone": "",
-    "name": "志凯"
-  },
-  {
-    "phone": "",
-    "name": "河岳"
-  },
-  {
-    "phone": "",
-    "name": "林以"
-  },
-  {
-    "phone": "",
-    "name": "此彼"
-  },
-  {
-    "phone": "",
-    "name": "淼缈"
-  },
-]
+var member = [{
+  "phone": "",
+  "name": "栈长"
+},
+{
+  "phone": "",
+  "name": "库特"
+},
+{
+  "phone": "",
+  "name": "明程"
+},
+{
+  "phone": "",
+  "name": "空寻"
+},
+{
+  "phone": "",
+  "name": "干拉"
+},
+{
+  "phone": "",
+  "name": "骛望"
+},
+{
+  "phone": "",
+  "name": "晟一"
+},
+{
+  "phone": "",
+  "name": "戒律"
+},
+{
+  "phone": "",
+  "name": "织弦"
+},
+{
+  "phone": "",
+  "name": "承武"
+},
+{
+  "phone": "",
+  "name": "超六"
+},
+{
+  "phone": "",
+  "name": "艾许"
+},
+{
+  "phone": "",
+  "name": "亚诺"
+},
+{
+  "phone": "",
+  "name": "金鞍"
+},
+{
+  "phone": "",
+  "name": "沙匠"
+},
+{
+  "phone": "",
+  "name": "白焱"
+},
+{
+  "phone": "",
+  "name": "临野"
+},
+{
+  "phone": "",
+  "name": "飏洋"
+},
+{
+  "phone": "",
+  "name": "柒伍"
+},
+{
+  "phone": "",
+  "name": "头孢"
+},
+{
+  "phone": "",
+  "name": "宁齐"
+},
+{
+  "phone": "",
+  "name": "木绣"
+},
+{
+  "phone": "",
+  "name": "川枫"
+},
+{
+  "phone": "",
+  "name": "志凯"
+},
+{
+  "phone": "",
+  "name": "河岳"
+},
+{
+  "phone": "",
+  "name": "此彼"
+},
+{
+  "phone": "",
+  "name": "淼缈"
+},]
